c26bcec9c94532ace514f2a463d3908a5a3c7ec7
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2026-03-19 17:28:37 -0300

    feat: create Company model

---
+++ BackEnd/api/src/main/java/com/labareda/api/domain/model/Company.java
@@ -0,0 +1,22 @@
+package com.labareda.api.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "company")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+}
