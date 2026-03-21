d213b5cabcdc96bb33595f263e8b72a68b69d417
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2026-03-21 15:46:07 -0300

    feat: add JWT security (TokenService, SecurityFilter and SecurityConfig)

---
+++ BackEnd/api/src/main/java/com/labareda/api/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package com.labareda.api.security;
+
+
+import com.labareda.api.domain.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if(token != null) {
+            String email = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByEmail(email);
+
+            if(user != null) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null,user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
---
+++ BackEnd/api/src/main/java/com/labareda/api/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.labareda.api.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.labareda.api.domain.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("labareda-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(genareteExpirationDate())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar token JWT", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("labareda-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTCreationException e) {
+            return "";
+        }
+    }
+
+    private Instant genareteExpirationDate() {
+        return LocalDateTime.now().plusHours(8).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
